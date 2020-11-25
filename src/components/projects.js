0530e55a146b56f2cc02395cdaefd67ad8aee5cb
matthewlazewski <matthew.lazewski@gmail.com>
2020-11-25 12:34:07 -0600

    added background image

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { GrGithub } from 'react-icons/gr';
 
 const Projects = () => {
     return (
         <div>
             <div>
                 <h1 align='center'><b>Projects:</b></h1>
+            </div>
+            <div className="projects">
+                <a href="https://github.com/matthewlazewski/friendhub-frontend"><GrGithub /> </a><br></br>
+                <a href="https://github.com/matthewlazewski/on-par-project"><GrGithub /> </a><br></br>
+                <a href="https://github.com/matthewlazewski/my_workout_finder"><GrGithub /> </a>
             </div>
         </div>
     )

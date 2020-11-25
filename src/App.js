6a6b8f8062c9dfed8443a4709cd9a346a19406ec
matthewlazewski <matthew.lazewski@gmail.com>
2020-11-25 10:58:20 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Footer from './components/footer';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 import Certification from './components/certification';
 import Projects from './components/projects';
 import headshot from './images/headshot.jpg';
@@ -8,9 +9,9 @@ const App = () => {
   return (
     <div className = "App">
       <div className="wrapper">
-        <img src={headshot} alt="headshot" width='300' height='300' />
-      </div>
-      <div>
+        <div>
+          <img src={headshot} alt="headshot" width='300' height='300' />
+        </div>
         <Certification/>
         <Projects />
         <Footer/>

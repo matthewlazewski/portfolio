56a7ca8d3eabb717ac19b045132a61a63f63ecd2
matthewlazewski <matthew.lazewski@gmail.com>
2020-11-25 10:31:23 -0600

    added headshot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Footer from './components/footer'
 import './App.css';
 import Certification from './components/certification';
+import headshot from './images/headshot.jpg'
 
 const App = () => {
   return (
     <div className = "App">
       <div className="wrapper">
-        <h1>Matt Lazewski</h1>
+        <img src={headshot} alt="headshot" width='300' height='300' />
       </div>
       <div>
         <Certification/>
--- src/components/certification.js
+++ src/components/certification.js
@@ -6,7 +6,12 @@ import pdf from '../images/certificate.pdf'
 const Certification = () => {
     return (
         <div>
-            <iframe align='center' width='500' height='300' src={pdf} title='Certificate' />
+            <div class="info">
+                <p>My name is Matt Lazewski. I currently live in Oklahoma City. I am hoping to make my way into the tech world. I have recently completed the Full Stack Software Engineering Bootcamp at Flatiron School. My knowledge includes JavaScript, React, Redux, HTML5, CSS, Ruby and SQL, but I am learning more every day.</p>
+            </div>
+            <div class="certficate">
+                <iframe align='center' width='500' height='300' src={pdf} title='Certificate' />
+            </div>
         </div>
     )
 };
